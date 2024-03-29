76c34a9b0ab812665de53ea666a8abd12766271b
zesskaaa <161881021+zesskaaa@users.noreply.github.com>
2024-03-29 13:44:09 +0300

    Update лабораторная 2(часть1).py

---
+++ лабораторная 2(часть1).py
@@ -0,0 +1,14 @@
+x=int(input ())
+if-6<=x<-1:
+    print ("true")
+else:
+    print("false")
+
+
+x=int(input())
+if -8<x<3:
+    print ("true")
+elif 10<=x<=15:
+    print("true")
+else:
+    print("false")
